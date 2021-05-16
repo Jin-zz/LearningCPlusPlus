7cb3e796ab84dddc3c9756af2960235f9352fb35
stacejin <stacejin@tencent.com>
2021-05-16 19:17:18 +0800

    feat:note updata

--- Code/Chapter3/main.cpp
+++ Code/Chapter3/main.cpp
@@ -21,6 +21,21 @@ int main() {
 //    iteratorUsing();
 
     cout << "----[array Test]----" << endl;
-    arrayTest();
+//    arrayTest();
+
+    int x[10];
+    int *p = x;
+
+    cout<< sizeof(x)<<endl;
+    cout<< sizeof(*x)<<endl;
+    cout << sizeof(x)/sizeof(*x) << endl;
+
+    cout<< sizeof(p)<<endl;
+    cout<< sizeof(*p)<<endl;
+    cout << sizeof(p)/sizeof(*p) << endl;
+
+    double *q = new double(100);
+    cout<< sizeof q;
+
     return 0;
 }
